4b1f2114ffe9e7179e1633424814ec0f7a2fbc68
PrabhavKarve <karve.prabhav@gmail.com>
2024-12-27 23:14:38 -0500

    CORS issue

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const collection = require("./mongo");
 const cors = require("cors");
 const app = express();
 
+// Allow requests from all origins
+app.use(cors({
+    origin: '*', // This allows requests from any origin
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -59,5 +64,4 @@ app.post("/signup", async (req, res) => {
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
-    return "Server is running on port 8000"
 });
